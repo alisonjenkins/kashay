68b83488a447c337ef6e6372bf3213a0943f360c
Alan Jenkins <alan.james.jenkins@gmail.com>
2022-11-14 16:12:24 +0000

    Ensure stdout is flushed

--- src/app/application.rs
+++ src/app/application.rs
@@ -8,5 +8,6 @@ pub async fn run() -> Result<()> {
     let args = CliArgs::parse();
     let creds = get_eks_token(&args.cluster_name, &args.skip_cache, &args.role_arn).await?;
     tokio::io::stdout().write_all(creds.as_bytes()).await?;
+    tokio::io::stdout().flush().await?;
     Ok(())
 }
