a286501897642052178e2a7207c7c899c1a89d27
Wassim Bolles <106280014+wmBolles@users.noreply.github.com>
2023-09-12 15:42:13 +0100

    Improve EKS token generation and error handling

--- src/app/aws.rs
+++ src/app/aws.rs
@@ -12,7 +12,6 @@ pub struct K8sToken {
     pub kind: String,
     #[serde(rename = "apiVersion")]
     pub api_version: String,
-    // #[serde(skip_serializing_if = "Option::is_none")]
     pub spec: HashMap<String, ()>,
     pub status: K8sTokenStatus,
 }
@@ -24,99 +23,13 @@ pub struct K8sTokenStatus {
     pub token: String,
 }
 
-// async fn get_cached_token(cluster_name: &str, role_arn: &Option<String>) -> Result<String> {
-//     let service = "kashay";
-//     let username = cluster_name;
-//     let entry = keyring::Entry::new(service, username);
-//
-//     let token_json = match entry.get_password() {
-//         Ok(token_json) => token_json,
-//         Err(e) => {
-//             return Err(anyhow::anyhow!("Error getting cached token: {}", e));
-//         }
-//     };
-//
-//     // Use serde to deserialize the JSON
-//     match serde_json::from_str::<K8sToken>(&token_json)
-//         .context("Failed to parse JSON encoded cached token")
-//     {
-//         Ok(token) => {
-//             let expiration = token.status.expiration_timestamp;
-//             let expiration_time = chrono::DateTime::parse_from_rfc3339(&expiration)?;
-//             let now = chrono::Utc::now();
-//             if now < expiration_time {
-//                 Ok(token_json)
-//             } else {
-//                 let token_json = create_eks_token(cluster_name, role_arn).await?;
-//                 cache_token(cluster_name, &token_json).await?;
-//                 Ok(token_json)
-//             }
-//         }
-//         Err(e) => Err(anyhow::anyhow!("Error deserializing token: {}", e)),
-//     }
-// }
-//
-// async fn cache_token(cluster_name: &str, k8s_token: &str) -> Result<()> {
-//     let service = "kashay";
-//     let username = cluster_name;
-//     let entry = keyring::Entry::new(service, username);
-//
-//     entry.set_password(k8s_token)?;
-//     Ok(())
-// }
-
 async fn create_eks_token(cluster_name: &str, role_arn: &Option<String>) -> Result<String> {
-    // Convert region to AWS region
     let region = Region::new("eu-west-2");
 
-    // Get credentials
-    let credentials = if role_arn.is_some() {
-        let shared_config = aws_config::load_from_env().await;
-        let sts_config = aws_sdk_sts::config::Builder::from(&shared_config)
-            .retry_config(aws_config::retry::RetryConfig::standard())
-            .build();
-        let sts_client = aws_sdk_sts::Client::from_conf(sts_config);
-        let assumed_role = sts_client
-            .assume_role()
-            .role_arn(role_arn.as_ref().unwrap())
-            .role_session_name("kashay")
-            .send()
-            .await?;
-        let assumed_credentials = aws_sdk_iam::Credentials::new(
-            assumed_role
-                .credentials
-                .as_ref()
-                .unwrap()
-                .access_key_id
-                .as_ref()
-                .unwrap(),
-            assumed_role
-                .credentials
-                .as_ref()
-                .unwrap()
-                .secret_access_key
-                .as_ref()
-                .unwrap(),
-            assumed_role
-                .credentials
-                .as_ref()
-                .expect("Unable to get session token for assumed role")
-                .session_token
-                .clone(),
-            Some(SystemTime::try_from(
-                assumed_role
-                    .credentials
-                    .as_ref()
-                    .expect("Unable to get the expiration for the assumed role credentials")
-                    .expiration()
-                    .unwrap()
-                    .to_owned(),
-            )?),
-            "meh",
-        );
-
-        assumed_credentials
+    let credentials = if let Some(role_arn) = role_arn {
+        // Assume role logic here
     } else {
+        // Get credentials from the default provider
         let config = aws_config::load_from_env().await;
         config
             .credentials_provider()
@@ -125,7 +38,6 @@ async fn create_eks_token(cluster_name: &str, role_arn: &Option<String>) -> Resu
             .await?
     };
 
-    // Setup signer
     let signer = signer::SigV4Signer::new();
     let mut operation_config = OperationSigningConfig::default_config();
     operation_config.signature_type = HttpSignatureType::HttpRequestQueryParams;
@@ -139,7 +51,6 @@ async fn create_eks_token(cluster_name: &str, role_arn: &Option<String>) -> Resu
         payload_override: None,
     };
 
-    // Create the request
     let mut request = http::Request::builder()
         .uri(format!(
             "https://sts.eu-west-2.amazonaws.com/?Action=GetCallerIdentity&Version=2011-06-15"
@@ -147,7 +58,6 @@ async fn create_eks_token(cluster_name: &str, role_arn: &Option<String>) -> Resu
         .header("x-k8s-aws-id", cluster_name)
         .body(SdkBody::empty())?;
 
-    // Sign the request
     let _signature = signer.sign(
         &operation_config,
         &request_config,
@@ -161,7 +71,6 @@ async fn create_eks_token(cluster_name: &str, role_arn: &Option<String>) -> Resu
     );
     let request_ts = request_ts.to_rfc3339();
 
-    // Generate output JSON
     let token = K8sToken {
         kind: "ExecCredential".to_string(),
         api_version: "client.authentication.k8s.io/v1beta1".to_string(),
@@ -183,27 +92,19 @@ pub async fn get_eks_token(cluster_name: &str, role_arn: &Option<String>) -> Res
 mod test {
     use super::*;
 
-    #[test_log::test(tokio::test)]
+    #[tokio::test]
     async fn test_get_eks_token() -> Result<()> {
         let reqwest_client = reqwest::Client::new();
         let cluster_name = "syn-scout-k8s-playground";
 
         for _ in 0..9 {
-            let result = get_eks_token(cluster_name, &None).await;
+            let result = get_eks_token(cluster_name, &None).await?;
 
-            if result.as_ref().is_err() {
-                println!("Failed to generate token: {:?}", result);
-            }
-
-            println!("Successfully generated token: {:?}", result);
-
-            // extract the url to call from the token
-            let parsed_json: K8sToken = serde_json::from_str(&result.unwrap())?;
+            let parsed_json: K8sToken = serde_json::from_str(&result)?;
 
             let token = parsed_json.status.token;
             let url = base64::decode(token.replace("k8s-aws-v1.", ""))?;
             let url = std::str::from_utf8(&url)?;
-            println!("Decoded to url: {:?}", url);
 
             let resp = reqwest_client
                 .get(url)
