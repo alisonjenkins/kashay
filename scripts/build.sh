3090f14de97ba966195f190b1c0c1646beb29c99
Alison Jenkins <alison.jenkins@brambles.com>
2025-02-28 09:38:30 +0000

    Add some debug ls commands to build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,15 @@ export ARTIFACT_NAME="eks-creds-$1"
 cargo build --release --locked --target "$1"
 
 # Create the artifact
+echo "Working dir content:"
+ls -la
+
+echo "target dir content:"
+ls -la target
+
+echo "target/release dir content:"
+ls -la target/release
+
 cp "target/release/eks-creds" "$ARTIFACT_NAME"
 cp README.md LICENSE "$ARTIFACT_NAME"
 
