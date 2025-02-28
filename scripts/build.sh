f95feac8091ba2514ef63cd2d2bf641e3481c411
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-02-28 07:48:09 +0000

    Add build.sh script

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+export ARTIFACT_NAME="eks-creds-$1"
+
+# Build for the target
+cargo build --release --locked --target "$1"
+
+# Create the artifact
+cp "target/$1/release/eks-creds" "$ARTIFACT_NAME"
+cp README.md LICENSE "$ARTIFACT_NAME"
+
+# Zip the artifact
+if ! command -v zip &>/dev/null; then
+    sudo apt-get update && sudo apt-get install -yq zip
+fi
+
+zip -r "$ARTIFACT_NAME.zip" "$ARTIFACT_NAME"
